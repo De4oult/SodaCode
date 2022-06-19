08402c31f84fc04e0fc62bd434d898c195487862
Timofey <timofej.dimitrov@yandex.ru>
2022-06-19 12:27:46 +0300

    New Compiler Settings

--- src/com/de4oult/soda/Main.java
+++ src/com/de4oult/soda/Main.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
 
 import com.de4oult.soda.parser.Lexer;
 import com.de4oult.soda.parser.Parser;
@@ -14,15 +13,18 @@ import com.de4oult.soda.parser.ast.Statement;
 public final class Main {
 	
 	public static void main(String[] args) throws IOException {
-		System.out.print("Компилируемый файл >> ");
+		/*System.out.print("Компилируемый файл >> ");
 			
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		String pathToScript = sc.nextLine();
-		System.out.println("\n\n");
-		final String input = new String(Files.readAllBytes(Paths.get(pathToScript )), "UTF-8");
+		System.out.println("\n\n");*/
+		
+		String pathToScript = args[0];
+		final String input = new String(Files.readAllBytes(Paths.get(pathToScript)), "UTF-8");
 		final List<Token> tokens = new Lexer(input).tokenize();
 		final Statement program = new Parser(tokens).parse();
 		program.execute();
+		System.out.println();
 	}
 }
